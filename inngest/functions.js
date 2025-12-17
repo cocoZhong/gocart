1a2056b3a771c39c59b7fac55c55bb105ca128cf
Howard-Java <hanchaosocool@163.com>
2025-12-17 10:57:58 +0800

    first commit

---
+++ inngest/functions.js
@@ -0,0 +1,51 @@
+import prisma from "@/lib/prisma";
+import { inngest } from "./client";
+
+// inngest user function to save user data to database
+export const syncUserCreation = inngest.createFunction(
+    { id: "sync-user-create" },
+    { event: "clerk/user.created" },
+    async ({ event, step }) => {
+        const { data } = event;
+
+        await prisma.user.create({
+            data: {
+                id: data.id,
+                name: `${data.firstName} ${data.lastName}}`,
+                email: data.emailAddresses[0].emailAddress,
+                image: data.image_url,
+            },
+        });
+    }
+);
+export const syncUserUpdation = inngest.createFunction(
+    { id: "sync-user-update" },
+    { event: "clerk/user.updated" },
+    async ({ event, step }) => {
+        const { data } = event;
+
+        await prisma.user.update({
+            where: {
+                id: data.id,
+            },
+            data: {
+                name: `${data.first_name} ${data.last_name}}`,
+                email: data.email_addresses[0].email_address,
+                image: data.image_url,
+            },
+        });
+    }
+);
+export const syncUserDeletion = inngest.createFunction(
+    { id: "sync-user-delete" },
+    { event: "clerk/user.deleted" },
+    async ({ event, step }) => {
+        const { data } = event;
+
+        await prisma.user.delete({
+            where: {
+                id: data.id,
+            },
+        });
+    }
+);
